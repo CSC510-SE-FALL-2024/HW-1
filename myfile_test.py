6e1e2113bb6dcbe4529185c213ea455bf4629244
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-08-28 00:40:07 +0000

    added error

--- myfile_test.py
+++ myfile_test.py
@@ -1,6 +1,5 @@
-import pytest
 from myfile import is_prime
 def test_is_prime_pass():
     assert is_prime(5) == True
 def test_is_prime_fail():
-    assert is_prime(4) == True
+    assert is_prime(4) = True
