5ad930eee717c65152f334375701b6b79460b6d3
Niharika <43085656+nih326@users.noreply.github.com>
2024-08-28 01:03:15 +0000

    testing

--- myfile_test.py
+++ myfile_test.py
@@ -2,5 +2,5 @@
 from myfile import is_prime
 def test_is_prime_pass():
     assert is_prime(5) == True
-def test_is_prime_fail():
-    assert is_prime(4) == True
+#def test_is_prime_fail():
+    #assert is_prime(4) == True
