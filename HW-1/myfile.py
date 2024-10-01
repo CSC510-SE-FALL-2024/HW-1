8bc0eb7fe5a50abbd84e772f3c18d8aba79921a7
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-10-01 10:23:23 -0400

    Update myfile.py with docstrings

--- HW-1/myfile.py
+++ HW-1/myfile.py
@@ -1,4 +1,12 @@
+"""
+This module implements the funtion to determine if a number is prime or not.
+
+"""
 def is_prime(n):
+    """
+    This module implements prime number check
+
+    """
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
