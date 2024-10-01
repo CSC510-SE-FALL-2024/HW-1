be3f4af51dfb03547aab830d96c2e6ac152959bc
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-10-01 10:25:18 -0400

    Update myfile_test.py

--- HW-1/myfile_test.py
+++ HW-1/myfile_test.py
@@ -1,6 +1,17 @@
+"""
+This module implements testcases.
 
+"""
 from myfile import is_prime
 def test_is_prime_pass():
+    """
+    This module implements testcases.
+
+    """
     assert is_prime(6) == True
 def test_is_prime_fail():
+    """
+    This module implements testcases.
+
+    """
     assert is_prime(4) == True
